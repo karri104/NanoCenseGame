d0dfa8d482f3d4503aae3717ef26d521d32c5c0d
@jarvink7 <karri.jarvinen@aalto.fi>
2023-11-23 12:49:38 +0200

    Changed the program to use OOP.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
 import os
-import players
+from players import Game, Player
 
 
-def parse_file(filename):
+def parse_file(filename, game):
     cards = {}
     card_count = 0
     invalid_ids = []
@@ -12,49 +12,58 @@ def parse_file(filename):
     if idea == "":
         print("Empty file given")
     else:
+        parts = idea.split("\\n")
         duplicates = f.readline()
         i = 1
-        while idea != "" and duplicates != "":
+        while idea != [] and duplicates != "":
             duplicates = int(duplicates)
             if duplicates == 0:
                 invalid_ids.append(i)
-            cards[i] = [idea, duplicates]
+            parts = idea.split("\\n")
+            cards[i] = [parts, duplicates]
             card_count += duplicates
             idea = f.readline()
             duplicates = f.readline()
             i += 1
-    return cards, card_count, invalid_ids
+    game.cards = cards
+    game.card_count = card_count
+    game.invalid_ids = invalid_ids
 
 
-def pick_card(cards, card_count, invalid_ids):
-    card_id = random.randint(1, len(cards))
-    while card_id in invalid_ids:
-        card_id = random.randint(1, len(cards))
-    card = cards[card_id]
+def pick_card(game):
+    card_id = random.randint(1, len(game.cards))
+    while card_id in game.invalid_ids:
+        card_id = random.randint(1, len(game.cards))
+    card = game.cards[card_id]
     if card[1] != 0:
         card[1] -= 1
-        card_count -= 1
+        game.card_count -= 1
         if card[1] == 0:
-            invalid_ids.append(card_id)
-    return card[0], invalid_ids, card_count
-
+            game.invalid_ids.append(card_id)
+    return card
 
 def main():
+    game = Game()
+    name = input("Give player name. Continue with empty line:\n")
+    while name != "":
+        player = Player(name)
+        game.add_player(player)
+        name = input("Give player name. Continue with empty line:\n")
+
     filename = input("Give name of file containing card info:\n")
-    cards, card_count, invalid_ids = parse_file(filename)
+    parse_file(filename, game)
 
     draw = input("Press enter to draw a card.\n")
-    while draw == "" and card_count > 0:
-        card, invalid_ids, card_count = pick_card(cards, card_count, invalid_ids)
-        parts = card.split("\\n")
+    while draw == "" and game.card_count > 0:
+        card = pick_card(game)
         print("__________________________________________________________________________________________")
-        for part in parts:
+        for part in card[0]:
             print(part)
         print("----------")
         clear = input("Press enter to hide drawn card.\n")
         os.system('cls')
         print("----------")
-        print(f"Cards left in the deck: {card_count}\n")
+        print(f"Cards left in the deck: {game.card_count}\n")
         print("__________________________________________________________________________________________")
         draw = input("Press enter to draw another card.\n")
 
--- players.py
+++ players.py
@@ -3,16 +3,17 @@ class Game:
     def __init__(self):
         self.players = []
         self.cards = {}
+        self.card_count = 0
+        self.invalid_ids = []
+        self.turn = 0
 
     def add_player(self, player):
         self.players.append(player)
 
-    def update_cards(self, cards):
-        self.cards = cards
-
 
 class Player:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = ""
         self.cards = []
         self.cnts = 0
 
